726ab13f9a3325405376eec41175afa2ea7264f4
Samir Jadhav <samirzjadhav@gmail.com>
2024-03-23 10:14:17 -0700

    added fragment

--- src/components/FlightResults.js
+++ src/components/FlightResults.js
@@ -3,24 +3,26 @@ import React from "react";
 
 const FlightResults = ({ flights }) => {
   return (
-    <div
-      id="flight"
-      className="flight-results grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[20px] my-[50px] mx-[50px]"
-    >
-      {flights.map((flight) => (
-        <div
-          key={flight.id}
-          className="flight-card bg-gray-300 border p-[14px] rounded-md"
-        >
-          <p>Origin: {flight.origin}</p>
-          <p>Destination: {flight.destination}</p>
-          <p>Airline: {flight.airline}</p>
-          <p>Flight Number: {flight.flightNumber}</p>
-          <p>Arrival: {flight.arrivalTime}</p>
-          <p>Price: {flight.price}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div
+        id="flight"
+        className="flight-results grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[20px] my-[50px] mx-[50px]"
+      >
+        {flights.map((flight) => (
+          <div
+            key={flight.id}
+            className="flight-card bg-gray-300 border p-[14px] rounded-md"
+          >
+            <p>Origin: {flight.origin}</p>
+            <p>Destination: {flight.destination}</p>
+            <p>Airline: {flight.airline}</p>
+            <p>Flight Number: {flight.flightNumber}</p>
+            <p>Arrival: {flight.arrivalTime}</p>
+            <p>Price: {flight.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
